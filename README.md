7126b12461aa8743b3cbbd2ad61f8ad13cdb65b2
Evgenii Novikov <enovikov11@yandex.ru>
2026-08-12 23:00:41 +0200

    r3

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
-# tigor
-Personal public monorepo
-
-# etc
+# tigor monorepo
 
 Minimax h3
-
 mkpasswd -m yescrypt -R 11
 
 ssh-keygen -R 192.168.1.28
 ssh root@192.168.1.28
 
+nix build
 mkdir /root/mnt
 mount /dev/sde1 /root/mnt
+df -h /root/mnt
 cd /root/mnt/EFI/BOOT/
-mv BOOTX64.efi 2026-08-12_stateless-pre-r2_BOOTX64.efi
+mv BOOTX64.efi "$(date '+%Y-%m-%d_%H-%M-%S')_BOOTX64.efi"
 cp /root/result/BOOTX64.efi /root/mnt/EFI/BOOT/
 sync
 cd ~
 umount /root/mnt
 reboot now
+
+--
+
+add passwordless sudo
+pack flake nix into EFI as well
+Slow start
+Mouse
+Console - VM
+do nvidia can be present even without gui?
--- flake.nix
+++ flake.nix
@@ -18,7 +18,26 @@
     enableGnome = true;
     enableExtras = true; # Firefox and VSCodium
     enableTools = true;  # Hardware inspection and maintenance
-    enableVMs = true;    # QEMU, libvirt, virt-manager and passthrough
+    enableHostVirtualization = true; # QEMU, libvirt, virt-manager and passthrough
+    hostScalingFactor = 2;
+    enableGuestGui = true;
+    enableGuestScaling = true;
+    enableGuestNvidia = true; # Set true when passing the RTX PRO 6000 through
+    enableGuestContainers = true;
+    guestScalingFactor = 2;
+    guestDiskSize =
+      if enableGuestContainers then 32768
+      else if enableGuestGui || enableGuestNvidia then 16384
+      else 8192;
+    guestImageTags =
+      lib.optionals enableGuestGui [ "gui" ]
+      ++ lib.optionals (enableGuestGui && enableGuestScaling) [ "scl" ]
+      ++ lib.optionals enableGuestContainers [ "pod" ]
+      ++ lib.optionals enableGuestNvidia [ "nv" ];
+    guestImageBaseName =
+      "vm"
+      + lib.optionalString (guestImageTags != [ ])
+        "-${lib.concatStringsSep "-" guestImageTags}";
 
     commonPackages = pkgs: with pkgs; [
       curl
@@ -27,6 +46,7 @@
       tmux
       vim
       tree
+      wireguard-tools
     ];
 
     hostPackages = pkgs: with pkgs; [
@@ -51,6 +71,9 @@
       ethtool
       lm_sensors
       hdparm
+    ];
+
+    hostOnlyToolPackages = pkgs: with pkgs; [
       ipmitool
       efibootmgr
     ];
@@ -63,6 +86,125 @@
       passt
       virtiofsd
     ];
+
+    guestContainerPackages = pkgs: with pkgs; [
+      podman
+      podman-compose
+      gvisor # Provides runsc.
+    ];
+
+    gnomeModule = { scalingFactor, includeVMManager ? false }:
+      { lib, pkgs, ... }: {
+        hardware.graphics.enable = true;
+
+        services.xserver.enable = true;
+        services.displayManager.gdm.enable = true;
+        services.desktopManager.gnome.enable = true;
+
+        environment.gnome.excludePackages = with pkgs; [
+          gnome-backgrounds
+          gnome-bluetooth
+          gnome-color-manager
+          gnome-tour
+          gnome-user-docs
+          gnome-menus
+          orca
+        ];
+
+        hardware.bluetooth.enable = false;
+        services.hardware.bolt.enable = false;
+        i18n.inputMethod.enable = false;
+        services.avahi.enable = false;
+        services.colord.enable = false;
+        services.dleyna.enable = false;
+        services.geoclue2.enable = false;
+        services.power-profiles-daemon.enable = false;
+        services.orca.enable = false;
+        services.upower.enable = lib.mkForce false;
+        services.gnome = {
+          core-apps.enable = false;
+          evolution-data-server.enable = lib.mkForce false;
+          gcr-ssh-agent.enable = false;
+          gnome-browser-connector.enable = false;
+          gnome-initial-setup.enable = false;
+          gnome-keyring.enable = false;
+          gnome-online-accounts.enable = false;
+          gnome-remote-desktop.enable = false;
+          gnome-user-share.enable = false;
+          localsearch.enable = false;
+          rygel.enable = false;
+          tinysparql.enable = false;
+        };
+
+        services.pipewire = {
+          enable = true;
+          alsa.enable = true;
+          pulse.enable = true;
+        };
+
+        programs.gnome-disks.enable = true;
+        programs.firefox.enable = enableExtras;
+
+        environment.systemPackages =
+          gnomePackages pkgs
+          ++ lib.optionals enableExtras (extraPackages pkgs);
+
+        environment.sessionVariables = lib.optionalAttrs enableExtras {
+          NIXOS_OZONE_WL = "1";
+        };
+
+        xdg.mime.defaultApplications = {
+          "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
+        };
+
+        programs.dconf.profiles = {
+          gdm.databases = [{
+            settings = {
+              "org/gnome/desktop/interface".scaling-factor = lib.gvariant.mkUint32 scalingFactor;
+              "org/gnome/settings-daemon/plugins/power" = {
+                sleep-inactive-ac-type = "nothing";
+                sleep-inactive-battery-type = "nothing";
+              };
+            };
+          }];
+
+          user.databases = [{
+            settings = {
+              "org/gnome/desktop/interface".scaling-factor = lib.gvariant.mkUint32 scalingFactor;
+              "org/gnome/desktop/session".idle-delay = lib.gvariant.mkUint32 0;
+              "org/gnome/settings-daemon/plugins/housekeeping".donation-reminder-enabled = false;
+              "org/gnome/settings-daemon/plugins/power" = {
+                sleep-inactive-ac-type = "nothing";
+                sleep-inactive-battery-type = "nothing";
+              };
+              "org/gnome/shell".favorite-apps = [
+                "org.gnome.Nautilus.desktop"
+                "org.gnome.Console.desktop"
+                "org.gnome.DiskUtility.desktop"
+              ] ++ lib.optionals includeVMManager [ "virt-manager.desktop" ];
+            };
+          }];
+        };
+      };
+
+    guestNvidiaModule = { ... }: {
+      hardware.graphics.enable = true;
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia = {
+        branch = "production";
+        modesetting.enable = true;
+        open = true;
+        nvidiaSettings = enableGuestGui;
+      };
+    };
+
+    guestContainersModule = { pkgs, ... }: {
+      virtualisation.podman = {
+        enable = true;
+        extraRuntimes = [ pkgs.gvisor ];
+      };
+      environment.systemPackages = guestContainerPackages pkgs;
+    };
 
     commonModule = { pkgs, ... }: {
       time.timeZone = "Europe/Belgrade";
@@ -97,8 +239,13 @@
     stateless = lib.nixosSystem {
       inherit system;
 
-      modules = [
-        commonModule
+      modules =
+        [ commonModule ]
+        ++ lib.optional enableGnome (gnomeModule {
+          scalingFactor = hostScalingFactor;
+          includeVMManager = enableHostVirtualization;
+        })
+        ++ [
         ({ config, lib, pkgs, modulesPath, ... }: {
           # Four concurrent builds, each sized for one quarter of the
           # EPYC 7702P's 128 logical CPUs.
@@ -121,7 +268,7 @@
               hashedPassword = mainPassword;
               extraGroups =
                 [ "wheel" ]
-                ++ lib.optionals enableVMs [ "kvm" "libvirtd" ]
+                ++ lib.optionals enableHostVirtualization [ "kvm" "libvirtd" ]
                 ++ lib.optionals enableGnome [ "video" "render" ];
               openssh.authorizedKeys.keys = [ sshKey ];
             };
@@ -132,7 +279,7 @@
             (modulesPath + "/profiles/minimal.nix")
           ];
 
-          networking.hostName = "stateless-r2";
+          networking.hostName = "stateless-r3";
           networking.hostId = "06e694f9";
           networking.nftables.enable = true;
           networking.networkmanager.enable = false;
@@ -140,7 +287,7 @@
           boot.supportedFilesystems = [ "zfs" ];
 
           boot.initrd.kernelModules =
-            lib.optionals enableVMs [
+            lib.optionals enableHostVirtualization [
               "vfio_pci"
               "vfio"
               "vfio_iommu_type1"
@@ -150,13 +297,13 @@
               # VFIO modules precede it so the RTX PRO IDs are claimed first.
               "nouveau"
             ];
-          boot.kernelModules = lib.optionals enableVMs [ "kvm-amd" ];
+          boot.kernelModules = lib.optionals enableHostVirtualization [ "kvm-amd" ];
           boot.kernelParams =
             [
               "nohibernate"
               "modprobe.blacklist=ast"
             ]
-            ++ lib.optionals enableVMs [
+            ++ lib.optionals enableHostVirtualization [
               "amd_iommu=on"
               "iommu=pt"
               "iommu.strict=1"
@@ -171,97 +318,11 @@
 
           boot.zfs.forceImportRoot = false;
 
-          hardware.graphics.enable = enableGnome;
+          services.xserver.videoDrivers = lib.mkIf enableGnome [ "nouveau" ];
 
-          services.xserver = lib.mkIf enableGnome {
-            enable = true;
-            videoDrivers = [ "nouveau" ];
-          };
-          services.displayManager.gdm.enable = enableGnome;
-          services.desktopManager.gnome.enable = enableGnome;
+          programs.virt-manager.enable = enableHostVirtualization;
 
-          environment.gnome.excludePackages = lib.optionals enableGnome (with pkgs; [
-              gnome-backgrounds
-              gnome-bluetooth
-              gnome-color-manager
-              gnome-tour
-              gnome-user-docs
-              gnome-menus
-              orca
-            ]);
-
-          hardware.bluetooth.enable = false;
-          services.hardware.bolt.enable = false;
-          i18n.inputMethod.enable = false;
-          services.avahi.enable = false;
-          services.colord.enable = false;
-          services.dleyna.enable = false;
-          services.geoclue2.enable = false;
-          services.power-profiles-daemon.enable = false;
-          services.orca.enable = false;
-          services.upower.enable = lib.mkForce false;
-          services.gnome = lib.mkIf enableGnome {
-            core-apps.enable = false;
-            evolution-data-server.enable = lib.mkForce false;
-            gcr-ssh-agent.enable = false;
-            gnome-browser-connector.enable = false;
-            gnome-initial-setup.enable = false;
-            gnome-keyring.enable = false;
-            gnome-online-accounts.enable = false;
-            gnome-remote-desktop.enable = false;
-            gnome-user-share.enable = false;
-            localsearch.enable = false;
-            rygel.enable = false;
-            tinysparql.enable = false;
-          };
-
-          services.pipewire = {
-            enable = enableGnome;
-            alsa.enable = enableGnome;
-            pulse.enable = enableGnome;
-          };
-
-          programs.gnome-disks.enable = enableGnome;
-          programs.virt-manager.enable = enableVMs;
-          programs.firefox.enable = enableGnome && enableExtras;
-
-          environment.sessionVariables = lib.optionalAttrs (enableGnome && enableExtras) {
-            NIXOS_OZONE_WL = "1";
-          };
-
-          xdg.mime.defaultApplications = lib.optionalAttrs enableGnome {
-            "inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-          };
-          programs.dconf.profiles = lib.mkIf enableGnome {
-            gdm.databases = [{
-              settings = {
-                "org/gnome/desktop/interface".scaling-factor = lib.gvariant.mkUint32 2;
-                "org/gnome/settings-daemon/plugins/power" = {
-                  sleep-inactive-ac-type = "nothing";
-                  sleep-inactive-battery-type = "nothing";
-                };
-              };
-            }];
-
-            user.databases = [{
-              settings = {
-                "org/gnome/desktop/interface".scaling-factor = lib.gvariant.mkUint32 2;
-                "org/gnome/desktop/session".idle-delay = lib.gvariant.mkUint32 0;
-                "org/gnome/settings-daemon/plugins/housekeeping".donation-reminder-enabled = false;
-                "org/gnome/settings-daemon/plugins/power" = {
-                  sleep-inactive-ac-type = "nothing";
-                  sleep-inactive-battery-type = "nothing";
-                };
-                "org/gnome/shell".favorite-apps = [
-                  "org.gnome.Nautilus.desktop"
-                  "org.gnome.Console.desktop"
-                  "org.gnome.DiskUtility.desktop"
-                ] ++ lib.optionals enableVMs [ "virt-manager.desktop" ];
-              };
-            }];
-          };
-
-          virtualisation.libvirtd = lib.mkIf enableVMs {
+          virtualisation.libvirtd = lib.mkIf enableHostVirtualization {
             enable = true;
             onBoot = "ignore";
             onShutdown = "shutdown";
@@ -273,7 +334,7 @@
           };
 
           # Make libvirt 12.x's secret bootstrap self-contained on this stateless host.
-          systemd.services.virt-secret-init-encryption = lib.mkIf enableVMs {
+          systemd.services.virt-secret-init-encryption = lib.mkIf enableHostVirtualization {
             serviceConfig = {
               StateDirectory = "libvirt/secrets";
               StateDirectoryMode = "0700";
@@ -294,10 +355,10 @@
 
           environment.systemPackages =
             hostPackages pkgs
-            ++ lib.optionals enableGnome (gnomePackages pkgs)
-            ++ lib.optionals (enableGnome && enableExtras) (extraPackages pkgs)
-            ++ lib.optionals enableTools (toolPackages pkgs)
-            ++ lib.optionals enableVMs (vmHostPackages pkgs);
+            ++ lib.optionals enableTools (
+              toolPackages pkgs ++ hostOnlyToolPackages pkgs
+            )
+            ++ lib.optionals enableHostVirtualization (vmHostPackages pkgs);
 
           environment.shellAliases = {
             mnt = "zpool import -a && zfs load-key -a && zfs mount -a";
@@ -314,13 +375,32 @@
     vm = lib.nixosSystem {
       inherit system;
 
-      modules = [
-        commonModule
-        "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+      modules =
+        [
+          commonModule
+          "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+        ]
+        ++ lib.optional enableGuestGui (gnomeModule {
+          scalingFactor = if enableGuestScaling then guestScalingFactor else 1;
+        })
+        ++ lib.optional enableGuestNvidia guestNvidiaModule
+        ++ lib.optional enableGuestContainers guestContainersModule
+        ++ [
 
         ({ config, lib, modulesPath, pkgs, ... }: {
-          networking.hostName = "nixos-vm";
-          networking.useDHCP = lib.mkDefault true;
+          networking.hostName = "vm-r3";
+          networking.useDHCP = false;
+          networking.useNetworkd = true;
+          systemd.network = {
+            enable = true;
+            networks."10-dhcp" = {
+              matchConfig.Name = "en* eth*";
+              networkConfig = {
+                DHCP = "yes";
+                IPv6AcceptRA = true;
+              };
+            };
+          };
 
           boot.loader.grub = {
             enable = true;
@@ -343,7 +423,11 @@
           };
 
           services.qemuGuest.enable = true;
-          services.spice-vdagentd.enable = true;
+          services.spice-vdagentd.enable = enableGuestGui;
+          services.displayManager.autoLogin = lib.mkIf enableGuestGui {
+            enable = true;
+            user = "nixos";
+          };
 
           services.openssh = {
             openFirewall = true;
@@ -361,21 +445,25 @@
 
             nixos = {
               isNormalUser = true;
-              extraGroups = [ "wheel" ];
+              extraGroups =
+                [ "wheel" ]
+                ++ lib.optionals (enableGuestGui || enableGuestNvidia) [ "video" "render" ];
               hashedPassword = "";
               openssh.authorizedKeys.keys = [ sshKey ];
             };
           };
           security.sudo.wheelNeedsPassword = false;
 
+          environment.systemPackages = lib.optionals enableTools (toolPackages pkgs);
+
           environment.etc."nixos/flake.nix".source = ./flake.nix;
 
           system.build.qcow2 = builtins.import (modulesPath + "/../lib/make-disk-image.nix") {
             inherit config lib pkgs;
-            name = "nixos-virt-manager-image";
-            baseName = "nixos-virt-manager";
+            name = "${guestImageBaseName}-image";
+            baseName = guestImageBaseName;
             format = "qcow2";
-            diskSize = 8192;
+            diskSize = guestDiskSize;
             partitionTableType = "hybrid";
             copyChannel = false;
           };
